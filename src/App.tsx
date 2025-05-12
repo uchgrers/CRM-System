c6c6dbc639eddcc7f0c5cbc59445705e514d074c
uchgrers <uchgrers@gmail.com>
2025-05-12 14:47:04 +0300

    удаление ненужных комментариев

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,6 @@ import './App.css'
 import {Route, Routes} from "react-router-dom"
 import TodosPage from "./pages/TodosPage/TodosPage"
 
-// assets - иконки, картинки и тд
-// заменить перечисления на enum
-// имена типам давать в соответствии с докумнентацией
-// сделать мини ui либу (ui kit)
-
 function App() {
 
     return (

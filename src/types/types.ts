db5c6b9d58faf036e9e4172ac92de3baeb56ee62
uchgrers <uchgrers@gmail.com>
2025-05-12 15:49:04 +0300

    исправление ошибки enum ErrorMessageType и tsconfig.json

--- src/types/types.ts
+++ src/types/types.ts
@@ -20,7 +20,7 @@ export type TodoInfo = {
 export enum ErrorMessageType {
     TooShort = 'Task must contain at least 2 symbols',
     TooLong = 'Maximum task length is 64 symbols',
-    Correct = null
+    Correct = ''
 }
 
 export type GetTodos = {

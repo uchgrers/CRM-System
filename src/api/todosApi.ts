52574149671cb98fa31f4d671902b21dc452d6d4
uchgrers <uchgrers@gmail.com>
2025-05-28 13:34:33 +0300

    вынос объекта axios в отдельный файл

--- src/api/todosApi.ts
+++ src/api/todosApi.ts
@@ -1,16 +1,9 @@
-import axios from "axios"
 import {MetaResponse, Todo, TodoInfo, TodosStatus} from "../types/types"
-
-const todosInstance = axios.create({
-    baseURL: 'https://easydev.club/api/v1/',
-    headers: {
-        "Content-Type": "application/json",
-    }
-})
+import { apiInstance } from "./apiInstance"
 
 export const todosApi = {
     getTodos(todosStatus?: TodosStatus) {
-        return todosInstance.get<MetaResponse<Todo, TodoInfo>>(`todos?filter=${todosStatus}`)
+        return apiInstance.get<MetaResponse<Todo, TodoInfo>>(`todos?filter=${todosStatus}`)
             .then(response => response.data)
             .catch(error => {
                 console.log(error)
@@ -18,7 +11,7 @@ export const todosApi = {
             })
     },
     addTodo(title: string) {
-        return todosInstance.post<Todo | string>(`todos`, {title})
+        return apiInstance.post<Todo | string>(`todos`, {title})
             .then(response => response)
             .catch(error => {
                 console.log(error)
@@ -26,7 +19,7 @@ export const todosApi = {
             })
     },
     deleteTodo(id: number) {
-        return todosInstance.delete<string>(`todos/${id}`)
+        return apiInstance.delete<string>(`todos/${id}`)
             .then(response => response)
             .catch(error => {
                 console.log(error)
@@ -34,7 +27,7 @@ export const todosApi = {
             })
     },
     updateTodo(id: number, isDone: boolean, title: string) {
-        return todosInstance.put<Todo | string>(`todos/${id}`, {isDone, title})
+        return apiInstance.put<Todo | string>(`todos/${id}`, {isDone, title})
             .then(response => response)
             .catch(error => {
                 console.log(error)

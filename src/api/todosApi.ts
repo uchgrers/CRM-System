ef6fd3bd1c45a2efcf169bb48617a3b3b48a20d8
uchgrers <uchgrers@gmail.com>
2025-05-30 13:01:11 +0300

    рефакторинг апи

--- src/api/todosApi.ts
+++ src/api/todosApi.ts
@@ -2,36 +2,40 @@ import {MetaResponse, Todo, TodoInfo, TodosStatus} from "../types/types"
 import { apiInstance } from "./apiInstance"
 
 export const todosApi = {
-    getTodos(todosStatus?: TodosStatus) {
-        return apiInstance.get<MetaResponse<Todo, TodoInfo>>(`todos?filter=${todosStatus}`)
-            .then(response => response.data)
-            .catch(error => {
-                console.log(error)
-                throw new Error(String(error))
-            })
+    getTodos: async (todosStatus?: TodosStatus) => {
+        try {
+            const result = await apiInstance.get<MetaResponse<Todo, TodoInfo>>(`todos?filter=${todosStatus}`)
+            return result.data
+        } catch (error) {
+            console.log(error)
+            throw error
+        }
     },
-    addTodo(title: string) {
-        return apiInstance.post<Todo | string>(`todos`, {title})
-            .then(response => response)
-            .catch(error => {
-                console.log(error)
-                throw new Error(String(error))
-            })
+    addTodo: async (title: string) => {
+        try {
+            const result = await apiInstance.post<Todo | string>(`todos`, {title})
+            return result
+        } catch (error) {
+            console.log(error)
+            throw error
+        }
     },
-    deleteTodo(id: number) {
-        return apiInstance.delete<string>(`todos/${id}`)
-            .then(response => response)
-            .catch(error => {
-                console.log(error)
-                throw new Error(String(error))
-            })
+    deleteTodo: async (id: number) => {
+        try {
+            const result = await apiInstance.delete<string>(`todos/${id}`)
+            return result
+        } catch (error) {
+            console.log(error)
+            throw error
+        }
     },
-    updateTodo(id: number, isDone: boolean, title: string) {
-        return apiInstance.put<Todo | string>(`todos/${id}`, {isDone, title})
-            .then(response => response)
-            .catch(error => {
-                console.log(error)
-                throw new Error(String(error))
-            })
+    updateTodo: async (id: number, isDone: boolean, title: string) => {
+        try {
+            const result = await apiInstance.put<Todo | string>(`todos/${id}`, {isDone, title})
+            return result
+        } catch (error) {
+            console.log(error)
+            throw error
+        }
     }
 }
